f1d7908339c8d54d5fd0ffe8350b8967fea798db
HyperRush Network <hyperrushnetwork@gmail.com>
2025-08-09 20:56:02 +0200

    Update game-loader.js

--- assets/js/game-loader.js
+++ assets/js/game-loader.js
@@ -1,39 +1,43 @@
 <script>
 (function() {
-  function isRedirectStatus(status) {
-    return status >= 300 && status < 400;
+  // Wacht tot Module en Module.fetch bestaan
+  function waitForModuleFetch(maxTries = 50, interval = 100) {
+    return new Promise((resolve, reject) => {
+      let tries = 0;
+      const timer = setInterval(() => {
+        tries++;
+        if (typeof Module !== 'undefined' && typeof Module.fetch === 'function') {
+          clearInterval(timer);
+          resolve(Module.fetch);
+        } else if (tries >= maxTries) {
+          clearInterval(timer);
+          reject('Module.fetch niet gevonden na wachten');
+        }
+      }, interval);
+    });
   }
 
-  // Wacht tot Module en Module.fetch beschikbaar zijn
-  function hookUnityFetch() {
-    if (typeof Module !== 'undefined' && Module['fetch']) {
-      const originalFetch = Module['fetch'];
+  // Hook de fetch functie
+  waitForModuleFetch().then(originalFetch => {
+    Module.fetch = function(resource, options) {
+      options = options || {};
+      options.redirect = 'manual'; // voorkom automatische redirect opvolging
 
-      Module['fetch'] = function(resource, options) {
-        options = options || {};
-        // Forceer redirect mode op 'manual' om redirects niet automatisch te volgen
-        options.redirect = 'manual';
-
-        return originalFetch(resource, options).then(response => {
-          if (isRedirectStatus(response.status) || response.type === 'opaqueredirect') {
-            console.warn('Redirect gedetecteerd en geblokkeerd via Unity fetch:', response.url, 'status:', response.status);
-            // Redirect blokkeren door een rejected promise terug te geven
-            return Promise.reject(new Error('Redirect geblokkeerd: ' + response.url));
-          }
-          return response;
-        }).catch(err => {
-          console.error('Fout in fetch hook:', err);
-          throw err;
-        });
-      };
-
-      console.log('UnityLoader.js fetch functie succesvol gehooked voor redirect blokkering.');
-    } else {
-      // Module of Module.fetch nog niet beschikbaar? Probeer opnieuw over 100ms
-      setTimeout(hookUnityFetch, 100);
-    }
-  }
-
-  hookUnityFetch();
+      return originalFetch(resource, options).then(response => {
+        if ((response.status >= 300 && response.status < 400) || response.type === 'opaqueredirect') {
+          alert('Redirect gedetecteerd en geblokkeerd:\nURL: ' + response.url + '\nStatus: ' + response.status);
+          // Redirect blokkeren door error te gooien
+          return Promise.reject(new Error('Redirect geblokkeerd: ' + response.url));
+        }
+        return response;
+      }).catch(err => {
+        alert('Fetch hook error:\n' + err);
+        throw err;
+      });
+    };
+    alert('Module.fetch succesvol gehooked om redirects te blokkeren');
+  }).catch(err => {
+    alert('Error bij hooken Module.fetch:\n' + err);
+  });
 })();
 </script>
